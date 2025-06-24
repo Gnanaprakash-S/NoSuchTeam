1778f5376f72ede90c30ee4646f583c2abc58daf
2401217 <2401217@LTIN620335.cts.com>
2025-06-24 12:05:20 +0530

    Initial SetUp

---
+++ HealthScore/src/main/java/Cognizant/HealthScore/App.java
@@ -0,0 +1,10 @@
+package Cognizant.HealthScore;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ HealthScore/src/test/java/tests/TestScripts.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TestScripts 
+{
+	@Test
+	public void Hello()
+	{
+		
+	}
+}
