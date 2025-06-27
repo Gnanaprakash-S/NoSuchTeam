8fe58da4f1269a5a809fa20dd789fc706336133d
2401015 <2401015@LTIN619452.cts.com>
2025-06-27 18:40:16 +0530

    home_page done

--- HealthScore/src/main/java/pages/HomePage.java
+++ HealthScore/src/main/java/pages/HomePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 public class HomePage {
@@ -17,32 +18,39 @@ public class HomePage {
 	@FindBy(id="age")
 	WebElement age;
 	
+	@FindBy(id="pulse")
+	WebElement pulse;
+	
+	@FindBy(id="bp-systolic")
+	WebElement systolic;
+	
+	@FindBy(id="bp-diastolic")
+	WebElement diastolic;
+	
 	@FindBy(xpath ="//button[@type=\"submit\"]")
 	WebElement confirm;
-	
-	@FindBy(tagName="form")
-	WebElement form;
-	
 	
 	public HomePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
     
+	public void verify(WebElement element, String value) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+    	Boolean isValid = (Boolean) js.executeScript("return arguments[0].checkValidity();", element);
+    	
+    	if(!isValid)
+    		Assert.assertTrue(isValid,"Invalid input was accepted for value : "+value);
+    	
+    	else
+    		System.out.println("Valid input for value : "+value);
+	}
 
     public void nameField(String input) {
     	name.sendKeys(input);
     	confirm.click();
     	
-    	
-    	JavascriptExecutor js = (JavascriptExecutor) driver;
-    	Boolean isValid = (Boolean) js.executeScript("return arguments[0].checkValidity();", name);
-    	
-    	if(!isValid)
-    	Assert.assertTrue(isValid,"Invalid input was accepted for value : "+input);
-    	
-    	else
-    		System.out.println("Valid input for value : "+input);
+    	verify(name,input);
     }
     
     public void ageField(String nameInput,String ageInput) {
@@ -50,15 +58,41 @@ public class HomePage {
     	age.sendKeys(ageInput);
     	confirm.click();
     	
+    	verify(age,ageInput);
     	
-    	JavascriptExecutor js = (JavascriptExecutor) driver;
-    	Boolean isValid = (Boolean) js.executeScript("return arguments[0].checkValidity();", age);
+    }
+    
+    public void bpmField(String nameInput,String ageInput, String bpmInput) {
+    	name.sendKeys(nameInput);
+    	age.sendKeys(ageInput);
+    	Select sel = new Select(pulse);
+    	sel.selectByVisibleText(bpmInput);
+    	confirm.click();
     	
-    	if(!isValid)
-    	Assert.assertTrue(isValid,"Invalid input was accepted for value : "+ageInput);
+    	verify(pulse,bpmInput);
+    }
+    
+    public void systolicBP(String nameInput,String ageInput, String bpmInput, String systolicBPInput) {
+    	name.sendKeys(nameInput);
+    	age.sendKeys(ageInput);
+    	Select sel = new Select(pulse);
+    	sel.selectByVisibleText(bpmInput);
+    	systolic.sendKeys(systolicBPInput);
+    	confirm.click();
     	
-    	else
-    		System.out.println("Valid input for value : "+ageInput);
+    	verify(systolic,systolicBPInput);
+    }
+    
+    public void diastolicBP(String nameInput,String ageInput, String bpmInput, String systolicBPInput, String diastolicBPInput) {
+    	name.sendKeys(nameInput);
+    	age.sendKeys(ageInput);
+    	Select sel = new Select(pulse);
+    	sel.selectByVisibleText(bpmInput);
+    	systolic.sendKeys(systolicBPInput);
+    	diastolic.sendKeys(diastolicBPInput);
+    	confirm.click();
+    	
+    	verify(diastolic,diastolicBPInput);
     	
     }
 
