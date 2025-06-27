bd189f16e054601aaad6e7685a2af73ac4255364
2401014 <2401014@LTIN620066.cts.com>
2025-06-27 10:54:44 +0530

    deva request

--- HealthScore/src/test/java/tests/TestScripts.java
+++ HealthScore/src/test/java/tests/TestScripts.java
@@ -12,52 +12,35 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import pages.MainPage;
+import base.BaseTest;
+import pages.HomePage;
+import utils.ExcelRead;
 
 
-public class TestScripts 
-{
+public class TestScripts extends BaseTest{
+	
+	HomePage homePage;
+	ExcelRead excelData;
 
-	  WebDriver driver;
-    MainPage mainPage;
-
-    @BeforeTest
-    @Parameters("browser")
-    public void setUp(String browser) {
-    	if(browser.equalsIgnoreCase("chrome")) {
-    		driver = new ChromeDriver();
-    	}else if(browser.equalsIgnoreCase("edge")) {
-    		driver = new EdgeDriver();
-    	}
-        driver.manage().window().maximize();
-        driver.get("https://yalanuwu.github.io/Health-Index-Calculator/");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
-      
-        mainPage = new MainPage(driver);
-    }
-    
-
-    @AfterTest
-    public void closing() {
-        //driver.quit();
-        System.out.println("Test completed and browser closed.");
-    }
-=======
-	@Test
-	public void Hello()
+	@BeforeMethod
+	public void setUpPagesAndUtils()
 	{
-		System.out.println("Hello Team");
-		System.out.println("Bilal request");
-		System.out.println("deva request");
+		homePage = new HomePage(driver);
+		//excelData= new ExcelRead("filePath","sheetName");
+	}
+	
+	@Test(priority=0,dataProvider="filereader",dataProviderClass="utils.ExcelRead")
+	public void nameField()
+	{
 		
-		
-		
-		}
+	}
+	
+
 }
 
 
